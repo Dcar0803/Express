1f819f2b2933fd0a8458f3e7c1f9cf352ba817dd
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-07 21:41:01 -0500

    Updated: Added the createServer() function to the test file

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
-const app = require('../server');
+const { createServer } = require('../server');
 
 describe('GET /', () => {
-    
+
     test('It should respond with "hello world"', async () => {
+        const app = createServer();
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('hello world');
